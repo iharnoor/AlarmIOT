dd15ceaa8ce09302dbe5224f8722e61c843bb067
Harnoor <hsingh9@student.gsu.edu>
2019-11-05 14:46:12 -0500

    alarm functionality added

--- app/src/main/java/com/pawer/alarmiot/MainActivity.java
+++ app/src/main/java/com/pawer/alarmiot/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         Button btnCall = findViewById(R.id.btnCall);
 
         btnCall.setOnClickListener(
@@ -28,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
                         Intent callIntent = new Intent(Intent.ACTION_CALL);
                         callIntent.setData(Uri.parse("tel:6786870953"));
                         startActivity(callIntent);
+                    }
+                }
+        );
+
+        Button btnAlarm = findViewById(R.id.btnAlarm);
+
+        btnAlarm.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.xyz);
+                        mediaPlayer.start();
                     }
                 }
         );
