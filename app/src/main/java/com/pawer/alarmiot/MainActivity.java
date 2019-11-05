80a0c9f5d5d5824f06883728904e0a1da9eb57a5
Harnoor <hsingh9@student.gsu.edu>
2019-11-05 13:20:20 -0500

    call button feature added

--- app/src/main/java/com/pawer/alarmiot/MainActivity.java
+++ app/src/main/java/com/pawer/alarmiot/MainActivity.java
@@ -1,7 +1,16 @@
 package com.pawer.alarmiot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btnCall = findViewById(R.id.btnCall);
+
+        btnCall.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent callIntent = new Intent(Intent.ACTION_CALL);
+                        callIntent.setData(Uri.parse("tel:6786870953"));
+                        startActivity(callIntent);
+                    }
+                }
+        );
     }
 }
